f168ade548e494876b348987e220c3de19ae8284
MateusLuisMk19 <95911492+MateusLuisMk19@users.noreply.github.com>
2022-10-04 02:12:43 +0100

    enter

--- JS/index.js
+++ JS/index.js
@@ -1,20 +1,28 @@
-function salut() {}
+function salut() {
+  var data = new Date();
+  var hora = data.getHours();
+  var texto = document.getElementById("salut");
+
+  if (hora > 5 && hora < 12) {
+    texto.innerHTML = "Bom Dia!";
+  }
+  if (hora > 11 && hora < 18) {
+    texto.innerHTML = "Boa Tarde!";
+  }
+  if (hora > 17 && hora <= 23) {
+    texto.innerHTML = "Boa Noite!";
+  }
+  if (hora >= 0 && hora < 6) {
+    texto.innerHTML = "Boa Noite / Madrugada!";
+  }
+}
 
 salut();
 
-var data = new Date();
-var hora = data.getHours();
-var texto = document.getElementById("salut");
+var altura = window.screen.height;
+var largura = window.screen.width;
+var imag = document.getElementById("img-1");
 
-if (hora > 5 && hora < 12) {
-  texto.innerHTML = "Bom Dia!";
-}
-if (hora > 11 && hora < 18) {
-  texto.innerHTML = "Boa Tarde!";
-}
-if (hora > 17 && hora <= 23) {
-  texto.innerHTML = "Boa Noite!";
-}
-if (hora >= 0 && hora < 6) {
-  texto.innerHTML = "Boa Noite / Madrugada!";
+if (altura > largura) {
+  imag.innerHTML += "<br/>";
 }
