b34555ab8a0992c54795fc76d4712a92a2b0ee4a
MateusLuisMk19 <95911492+MateusLuisMk19@users.noreply.github.com>
2022-10-29 19:50:28 +0100

    update

---
+++ GreaclosTest/3damas/brain64.js
@@ -0,0 +1,50 @@
+var red = 0;
+var green = 0;
+var blue = 0;
+let shadow = "#000";
+
+$(document).ready(() => {
+  paintField();
+});
+
+function paintField() {
+  for (let numLine = 8; numLine >= 1; numLine--) {
+    if (numLine % 2 == 0) {
+      for (let numPs = 1; numPs <= 8; numPs += 2) {
+        $(`#line${numLine} .ps-${numPs}`).css({
+          backgroundImage: `linear-gradient(45deg, ${shadow}, rgb(${red}, ${green}, ${blue}))`,
+        });
+      }
+    } else {
+      for (let numPs = 2; numPs <= 8; numPs += 2) {
+        $(`#line${numLine} .ps-${numPs}`).css({
+          backgroundImage: `linear-gradient(45deg, ${shadow}, rgb(${red}, ${green}, ${blue}))`,
+        });
+      }
+    }
+  }
+}
+
+function preview(value, ident) {
+  switch (ident) {
+    case "red":
+      red = value;
+      break;
+    case "blue":
+      blue = value;
+      break;
+    case "green":
+      green = value;
+      break;
+    default:
+      break;
+  }
+
+  console.log(red, green, blue);
+  paintField();
+}
+
+function changeShadow() {
+  shadow == "#000" ? (shadow = "#fff") : (shadow = "#000");
+  paintField();
+}
